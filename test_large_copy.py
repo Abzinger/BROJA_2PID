22b39b06e57aa748d73df3d009c8317be4a1e5f4
Dirk Oliver Theis <dotheisatutdotee@gmail.com>
2017-10-09 17:30:45 +0300

    added %-deviation from true (analytical) solution

--- test_large_copy.py
+++ test_large_copy.py
@@ -4,25 +4,30 @@ import broja_2pid as BROJA
 from broja_2pid import BROJA_2PID_Exception
 
 import time
+from math import log2
 
-n_Y = 10
-for n_Z in range(10,1000,10):
-    print("______________________________________________________________________")
-    print("COPY   with |Y| =",n_Y,", |Z| =",n_Z,":")
-    print("Create pdf.")
-    pdf = dict()
-    for y in range(n_Y):
-        for z in range(n_Z):
-            x = (y,z)
-            pdf[ (x,y,z) ] = 1./(n_Y*n_Z)
-        #^ for z
-    #^ for y
-    print("Run BROJA_2PID.pid().")
-    tic = time.process_time()
-    pid = BROJA.pid(pdf,output=0)
-    toc = time.process_time()
-    print("Partial information decomposition: ",pid)
-    print("Time: ",toc-tic,"sex")
-    Abdullah: Check whether result is correct :)
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-#^ for n_Z
+for n_Y in range(10,1000,10):
+    for n_Z in [10,25]:
+        print("______________________________________________________________________")
+        print("COPY   with |Y| =",n_Y,", |Z| =",n_Z,":")
+        print("Create pdf.")
+        pdf = dict()
+        for y in range(n_Y):
+            for z in range(n_Z):
+                x = (y,z)
+                pdf[ (x,y,z) ] = 1./(n_Y*n_Z)
+            #^ for z
+        #^ for y
+        print("Run BROJA_2PID.pid().")
+        tic = time.process_time()
+        pid = BROJA.pid(pdf,output=0)
+        toc = time.process_time()
+        print("Partial information decomposition: ",pid)
+        print("Deviation from analytical results:")
+        print("    UIY:", 100*abs(pid["UIY"]-log2(n_Y))/log2(n_Y), "%")
+        print("    UIZ:", 100*abs(pid["UIZ"]-log2(n_Z))/log2(n_Z), "%")
+        print("Time: ",toc-tic,"secs")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    #^ for n_Z
+#^ for n_Y
+#EOF
