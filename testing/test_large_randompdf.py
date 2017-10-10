cc584d0596645dd83c258edf6d6458438568b45e
Dirk Oliver Theis <dotheisatutdotee@gmail.com>
2017-10-10 16:49:56 +0300

    #iter

--- testing/test_large_randompdf.py
+++ testing/test_large_randompdf.py
@@ -11,17 +11,21 @@ from random import random
 from sys import argv
 
 print("test_large_randompdf.py -- part of BROJA_2PID (https://github.com/dot-at/BROJA_2PID/)")
-if len(argv) < 4:
-    print("Usage: python3 test_large_randompdf.py x y z")
-    print("Where:   x is the size of the range of X;")
-    print("Where:   y is the size of the range of Y;")
-    print("Where:   z is the size of the range of Z.")
+if len(argv) < 4 or len(argv)>5:
+    print("Usage: python3 test_large_randompdf.py x y z [iter]")
+    print("Where:   x    is the size of the range of X;")
+    print("         y    is the size of the range of Y;")
+    print("         z    is the size of the range of Z;")
+    print("         iter is the number of iterations")
+    print("              (defaults to 250).")
     exit(0)
 #^ if
 try:
     nX = int(argv[1])
     nY = int(argv[2])
     nZ = int(argv[3])
+    if len(argv)==5:    maxiter = int(argv[4])
+    else:               maxiter = 250
 except:
     print("I couldn't parse one of the arguments (they must all be integers)")
     exit(1)
@@ -32,8 +36,13 @@ if min(nX,nY,nZ) < 2:
     exit(1)
 #^ if
 
+if maxiter < 1:
+    print("# iterations must be >= 1.")
+    exit(1)
+#^ if
+
 tic = time.process_time()
-for iter in range(250):
+for iter in range(maxiter):
     print("Random PDFs   with |X| =",nX,"|Y| =",nY," |Z| =",nZ)
     print("______________________________________________________________________")
     print("Create pdf #",iter)
