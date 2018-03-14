b90911af3870808be5a206d50c7f5d6013587060
Abdullah Makkeh <abed@at01.cs.ut.ee>
2018-03-14 18:02:02 +0200

    editing test_and_gate

--- testing/test_and_gate.py
+++ testing/test_and_gate.py
@@ -16,6 +16,14 @@ print("Starting BROJA_2PID.pid() on AND gate.")
 try:
   returndict = pid(andgate, cone_solver="ECOS", output=2, **parms)
 
+  msg="""Shared information: {SI}
+  Unique information in Y: {UIY}
+  Unique information in Z: {UIZ}
+  Synergistic information: {CI}
+  Primal feasibility: {Num_err[0]}
+  Dual feasibility: {Num_err[1]}
+  Duality Gap: {Num_err[2]}"""
+  print(msg.format(**returndat))
   
 except BROJA_2PID_Exception:
   print("Cone Programming solver failed to find (near) optimal solution. Please report the input probability density function to abdullah.makkeh@gmail.com")
